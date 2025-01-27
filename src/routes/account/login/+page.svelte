80127291be9c43c75dbf824e9dc58223be00d638
sen-trie <122269172+sen-trie@users.noreply.github.com>
2025-01-26 19:12:17 -0500

    Artists: Added banners and profiles

--- src/routes/account/login/+page.svelte
+++ src/routes/account/login/+page.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
-    import { getSingle } from '$lib/db';
-    import { onMount } from 'svelte';
-    import { invalidate, invalidateAll } from '$app/navigation';
+    import { wrapDefault } from '$lib/db';
+    import { invalidate } from '$app/navigation';
     import { navigateTo } from '$lib/util';
-    import { page } from '$app/stores';
+    import { page } from '$app/state';
 
     function handleLogin(user_id: number) {
         window.localStorage.setItem('user_id', String(user_id));
         invalidate('user:profile').then(() => {
-            navigateTo('/', $page.url.pathname);
+            navigateTo('/', page.url.pathname);
         });
     }
 </script>
@@ -24,7 +23,7 @@
         class="card-container flex sm:max-w-[30%] flex-1 flex-col items-center justify-center group"
     >
         <img
-            src={getSingle('dp', profileImage)}
+            src={wrapDefault('dp', profileImage)}
             alt={profileName}
             class="mb-4 h-16 w-16 rounded-full shadow"
         />
@@ -39,11 +38,11 @@
 
 <div class="card-island">
     <h1 class="mb-2 text-center text-3xl font-bold">BrushHub Profiles</h1>
-    <p class="mb-6 text-center">Choose a profile to sign in:</p>
+    <p class="mb-6 text-center">Choose a profile to sign in with:</p>
     
     <div class="flex flex-col sm:flex-row justify-around gap-4">
-        {@render signButton(1, 'Artist', '1.jpg', 'Manage your commissions')}
-        {@render signButton(2, 'Commissioner', '3.png', 'Request and track services')}
-        {@render signButton(0, 'Guest', '3.png', 'Browse as a guest')}
+        {@render signButton(3, 'Artist', '3.jpg', 'Manage your commissions')}
+        {@render signButton(1, 'Commissioner', '1.jpg', 'Request and track services')}
+        {@render signButton(0, 'Guest', '', 'Browse as a guest')}
     </div>
 </div>
